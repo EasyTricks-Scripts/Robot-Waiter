fbda335d4463051761b4d6bfa96854eec472101d
EasyTricks-Scripts <caganprogamer11@gmail.com>
2025-03-13 20:45:33 +0300

    Add files via upload

---
+++ AI.py
@@ -0,0 +1,27 @@
+print("Welcome from AI Restuarant")
+ad=input("Can I take ur name please\n")
+print("Very Good Name",ad,"What do u want from special menu")
+print("""
+--Menu--
+  Soup 
+  Steak 
+  Fish 
+  Coke
+""")
+siparis=input()
+print("Hmmmm... Good choose",siparis,"I will give to your meal in a couple minute")
+print("Do you like the meal")
+sevmek=input()
+
+if ("sevmek == Yes"):
+    print("I like to hear that")
+else:
+    print("I am sorry about that")
+    
+    
+
+    
+
+
+      
+         
